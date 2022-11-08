5d2f3c8255a9af4604bffc498b45097117bd9bb0
tiaaan23 <sebaspallares@gmail.com>
2022-11-08 12:31:44 -0500

    12:31

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -61,7 +61,10 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x===y) {
+    return true;
+  }
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
