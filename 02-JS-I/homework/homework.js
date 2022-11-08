91073ab598ab429b911d30818f01f04a099acc9d
tiaaan23 <sebaspallares@gmail.com>
2022-11-08 11:21:52 -0500

    5 ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Sebas Pallares Dussan A POR VICTOR";
+const nuevaString = "Sebas Pallares Dussan";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 6;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = true; false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1.05;
 
 
 // En los próximos 22 problemas, deberás completar la función.
